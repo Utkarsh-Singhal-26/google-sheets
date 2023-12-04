9823fe9cca25b2dc61fb677a6e9fd47d4b4806f9
Utkarsh Singhal <singhalutkarsh26@gmail.com>
2023-12-04 20:10:36 +0530

    Changes to be committed: 	modified:   .gitignore 	modified:   package.json 	modified:   pnpm-lock.yaml 	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
 import React from "react";
+import { useEffect } from "react";
+import Tabletop from "tabletop.js";
 
 const App = () => {
+  const getData = async () => {
+    const sheet = await Tabletop.init({
+      key: import.meta.env.VITE_spreadsheetId,
+      sheetName: 'Sheet1',
+    });
+  
+    const data = await sheet.promise();
+    console.log(data);
+  };
+  
+  useEffect(() => {
+    getData();
+  }, []);
+
   return <div>App</div>;
 };
 
